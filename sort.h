5b5f49b5d65ac9a6b18068783f5dddf356c9cf36
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-02-07 21:08:41 +0400

    a

--- sort.h
+++ sort.h
@@ -1,45 +1,10 @@
-#ifndef DECK_H
-#define DECK_H
+#ifndef SORT_H
+#define SORT_H
 
-/**
- * enum kind_e - Card kind
- * @SPADE: Spades
- * @HEART: Hearts
- * @CLUB: Clubs
- * @DIAMOND: Diamonds
- */
-typedef enum kind_e
-{
-	SPADE = 0,
-	HEART,
-	CLUB,
-	DIAMOND
-} kind_t;
+#include <stddef.h>
 
-/**
- * struct card_s - Playing card
- * @value: Value of the card (Ace to King)
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-	const char *value;
-	const kind_t kind;
-} card_t;
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 
-/**
- * struct deck_node_s - Deck node
- * @card: Pointer to the card
- * @prev: Previous node
- * @next: Next node
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
+#endif /* SORT_H */
 
-void sort_deck(deck_node_t **deck);
-
-#endif
